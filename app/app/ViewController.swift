b90194010cc3d70adfdadea48bbb8bdaefc44870
Allison Hong <allisonhong1023@berkeley.edu>
2022-09-23 19:56:11 -0700

    create xcode proj

---
+++ app/app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  app
+//
+//  Created by Allison on 9/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
