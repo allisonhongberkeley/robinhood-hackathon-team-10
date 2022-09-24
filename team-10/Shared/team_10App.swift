9e40b40bf3dff139f274963e437527e7cb346309
Allison Hong <allisonhong1023@berkeley.edu>
2022-09-23 19:47:24 -0700

    create xcode project

---
+++ team-10/Shared/team_10App.swift
@@ -0,0 +1,17 @@
+//
+//  team_10App.swift
+//  Shared
+//
+//  Created by Allison on 9/23/22.
+//
+
+import SwiftUI
+
+@main
+struct team_10App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
